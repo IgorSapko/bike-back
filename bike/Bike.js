1eecb57bf426807fe95f24451a5c1804f506b877
Ior Sapko <galvatron260781@gmail.com>
2021-08-01 16:02:29 +0300

    14th

--- bike/Bike.js
+++ bike/Bike.js
@@ -1,34 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const BikeSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    type: {
-        type: String,
-        required: true,
-    },
-    color: {
-        type: String,
-        required: true,
-    },
-    wheelSize: {
-        type: Number,
-        required: true,
-    },
-    price: {
-        type: Number,
-        required: true,
-    },
-    description: {
-        type: String,
-        // required: true,
-    },
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+  wheelSize: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    // required: true,
+  },
+  status: {
+    type: String,
+    default: "Available",
+  },
 });
 
 // bikes
-const Bike = mongoose.model('Bike', BikeSchema);
+const Bike = mongoose.model("Bike", BikeSchema);
 
 module.exports = Bike;
--- bike/bike.controller.js
+++ bike/bike.controller.js
@@ -3,7 +3,7 @@ const Bike = require("./Bike");
 async function getBikes(req, res) {
   try {
     const bikes = await Bike.find();
-    res.json(bikes);
+    res.status(200).json(bikes);
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -12,9 +12,6 @@ async function getBikes(req, res) {
 async function createBike(req, res) {
   try {
     const bike = await Bike.create(req.body);
-    // res.json(bike);
-    // const bikeColllection = await getBikes(req, res)
-
     res.status(201).json(bike);
   } catch (error) {
     res.status(500).send(error.message);
@@ -26,14 +23,12 @@ async function deleteBike(req, res) {
     const {
       params: { id },
     } = req;
-
     const bike = await User.findByIdAndDelete(id);
-
     if (!bike) {
       return res.status(404).send("Bike is not found");
     }
     res.json(bike);
-      } catch (error) {
+  } catch (error) {
     res.status(500).send(error.message);
   }
 }
