4248e732281b2f4c57c3b344ce33734e639f7fc0
Ior Sapko <galvatron260781@gmail.com>
2021-08-04 01:00:43 +0300

    28th

--- bike/bike.controller.js
+++ bike/bike.controller.js
@@ -4,16 +4,52 @@ const Joi = require("joi");
 async function getBikes(req, res) {
   try {
     const bikes = await Bike.find();
-    res.status(200).json(bikes);
+    const statistics = await getStatistics();
+    res.status(200).json({
+      bikes,
+      statistics,
+    });
   } catch (error) {
     res.status(500).send(error.message);
   }
+}
+async function getStatistics() {
+  const totalQuantityBikes = await Bike.find().countDocuments();
+  const avialableBikesQuantity = await Bike.find({
+    status: "Available",
+  }).countDocuments();
+  const busyBikesQuantity = await Bike.find({
+    status: "Busy",
+  }).countDocuments();
+
+  const resultTotalPrice = await Bike.aggregate([
+    {
+      $group: {
+        _id: null,
+        total: {
+          $sum: "$price",
+        },
+      },
+    },
+  ]);
+  const averageCost = resultTotalPrice[0].total / totalQuantityBikes;
+  return {
+    totalQuantityBikes,
+    avialableBikesQuantity,
+    busyBikesQuantity,
+    averageCost,
+  };
 }
 
 async function createBike(req, res) {
   try {
     const bike = await Bike.create(req.body);
-    res.status(201).json(bike);
+    const statistics = await getStatistics();
+    res.status(200).json({
+      bike,
+      statistics,
+    });
+    // res.status(201).json(bike);
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -21,7 +57,6 @@ async function createBike(req, res) {
 
 async function updateBike(req, res) {
   try {
-    console.log(req.body);
     const bike = await Bike.findByIdAndUpdate(
       req.body.id,
       { $set: { status: req.body.status } },
@@ -29,7 +64,12 @@ async function updateBike(req, res) {
         new: true,
       }
     );
-    res.status(201).json(bike);
+    const statistics = await getStatistics();
+    res.status(200).json({
+      bike,
+      statistics,
+    });
+    // res.status(201).json(bike);
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -41,7 +81,8 @@ async function deleteBike(req, res) {
     if (!bike) {
       return res.status(404).send("Bike is not found");
     }
-    res.json(bike);
+    const statistics = await getStatistics();
+    res.json({ bike, statistics });
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -49,13 +90,13 @@ async function deleteBike(req, res) {
 
 function validateData(req, res, next) {
   const validationRules = Joi.object({
-    name: Joi.string().required(),
-    type: Joi.string().required(),
-    color: Joi.string().required(),
+    name: Joi.string().required().min(5),
+    type: Joi.string().required().min(5),
+    color: Joi.string().required().min(5),
     wheelSize: Joi.number().required(),
     price: Joi.number().required(),
-    id: Joi.string().required(),
-    description: Joi.string().required(),
+    id: Joi.string().required().min(5),
+    description: Joi.string().required().min(5),
   });
   const validationResult = validationRules.validate(req.body);
   if (validationResult.error) {
